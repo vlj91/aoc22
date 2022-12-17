c2298a47e83b40d80953c30bdef53ce1cea2606f
Vaughan Jones <vlj7491@gmail.com>
2022-12-17 12:06:37 +1100

    day one part two

--- days/1/solution.rb
+++ days/1/solution.rb
@@ -2,4 +2,6 @@ elves = File.read("input.txt").split("\n\n").map do |elf|
   elf.split("\n").sum(&:to_i)
 end
 
-puts elves.sort.last
+puts "Part one: #{elves.sort.last}"
+
+puts "Part two: #{elves.sort.reverse.take(3).sum}"
